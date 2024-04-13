1423e291ea6896316f7bc0a738bacce680cc4719
mesepith <zahiralam29@gmail.com>
2024-04-13 16:38:06 +0530

    share option

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useCallback } from 'react';
+import { View, Text, TouchableOpacity, Share } from 'react-native';
 import styles from './Message.styles';
 
 interface MessageProps {
@@ -7,15 +7,29 @@ interface MessageProps {
   response: string;
 }
 
-const Message: React.FC<MessageProps> = ({ message, response }) => (
-  <View style={styles.container}>
-    <View style={styles.messageContainer}>
-      <Text style={styles.message}>{message}</Text>
+const Message: React.FC<MessageProps> = ({ message, response }) => {
+  const handleLongPress = useCallback(async () => {
+    try {
+      await Share.share({
+        message: `${response}`,
+      });
+    } catch (error) {
+      console.error('Error sharing message:', error);
+    }
+  }, [message, response]);
+
+  return (
+    <View style={styles.container}>
+        <View style={styles.messageContainer}>
+          <Text style={styles.message}>{message}</Text>
+        </View>
+      <TouchableOpacity onLongPress={handleLongPress}>
+      <View style={styles.responseContainer}>
+        <Text style={styles.response}>{response}</Text>
+      </View>
+      </TouchableOpacity>
     </View>
-    <View style={styles.responseContainer}>
-      <Text style={styles.response}>{response}</Text>
-    </View>
-  </View>
-);
+  );
+};
 
 export default Message;
