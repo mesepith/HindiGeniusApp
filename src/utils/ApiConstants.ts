1def08bd6578c89de54be21cb3f9c76e09bcb8db
mesepith <zahiralam29@gmail.com>
2024-04-17 12:51:38 +0530

    min

--- src/utils/ApiConstants.ts
+++ src/utils/ApiConstants.ts
@@ -1,4 +1,4 @@
-// const ADDRESS = '172.16.4.243';//off
-const ADDRESS = '172.20.10.9';//vi
+const ADDRESS = '172.16.4.243';//off
+// const ADDRESS = '172.20.10.9';//vi
 // const ADDRESS ='192.168.29.108';//SR
 export const API_BASE_URL='http://'+ADDRESS+':5029/api';
