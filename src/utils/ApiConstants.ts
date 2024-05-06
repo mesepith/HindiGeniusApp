63a74a71588373c9672cbdd4ab8765880fc7cdff
mesepith <zahiralam29@gmail.com>
2024-05-06 17:00:18 +0530

    Stop tracking src/utils/ApiConstants.ts

--- src/utils/ApiConstants.ts
+++
@@ -1,4 +0,0 @@
-const ADDRESS = 'http://172.16.4.243:5029';//off
-// const ADDRESS = 'http://172.20.10.9:5029';//vi
-// const ADDRESS ='http://192.168.29.108:5029';//SR
-export const API_BASE_URL=ADDRESS+'/api';
